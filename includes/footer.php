c8a509c7a436423eeed760b152a7a9a419888bd9
Vincent Blanca <Vince@MacBook-Pro-de-Vincent.local>
2022-05-30 21:20:42 +0200

    resolution bug visites

--- includes/footer.php
+++ includes/footer.php
@@ -2,10 +2,8 @@
         <div class="baspage">
             <div class="copyright">
                 ©&nbsp;Vincent&nbsp;Blanca&nbsp;2022&nbsp;&nbsp;Tous droits réservés&nbsp;&nbsp;(<a href="mailto:vincent.blanca@dgfip.finances.gouv.fr">Contact</a>)&nbsp;&nbsp;
-                <?php if(($_GET['action'] == 'accueil') OR (empty($_GET['action']))) :  ?> Nombre de visites : <?php include_once('./visites.php'); endif;?>
-            </div>
-            
+                <?php if($_GET['action'] == 'accueil') :  ?> Nombre de visites : <?php include('./visites.php'); endif;?>
+            </div>         
         </div>
-
     </body>
 </html>
