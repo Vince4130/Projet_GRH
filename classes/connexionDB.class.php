f2303d07eee943dcd9a4dcfe30d873a9c09da4a4
Vince4130 <vinceblanca@gmail.com>
2022-06-01 10:54:23 +0200

    connexdb

--- classes/connexionDB.class.php
+++ classes/connexionDB.class.php
@@ -5,10 +5,14 @@ class ConnexionDB {
 
     private static $instance = null;
     private $bdd;
+    public $port;
+    public $dbname;
+    public $user;
+    public $mpd;
 
     private function __construct()
     {
-      $this->bdd = new PDO("mysql:host=localhost:3306;dbname=grh;charset=UTF8","vince","413083", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+      $this->bdd = new PDO("mysql:host=localhost:8889;dbname=grh;charset=UTF8","root","root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
       
     }
 
