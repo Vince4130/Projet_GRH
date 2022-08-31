e9c8c92232e642c6e6cc5212e2e306f790aeda7c
Vince4130 <vinceblanca@gmail.com>
2022-08-31 17:09:04 +0200

    config connexion bdd

--- classes/connexionDB.class.php
+++ classes/connexionDB.class.php
@@ -5,17 +5,23 @@ class ConnexionDB {
 
     private static $instance = null;
     private $bdd;
-    public $port;
-    public $dbname;
-    public $user;
-    public $mpd;
+    public $host   = "localhost";
+    public $port   = 3308;
+    public $dbname ="grh";
+    public $user   = "vince";
+    public $mpd    = "bitonio";
 
     private function __construct()
     {
-       $this->bdd = new PDO("mysql:host=localhost:3306;dbname=grh;charset=UTF8","vince","413083", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-      // $this->bdd = new PDO("mysql:host=localhost:8889;dbname=grh_dev030622;charset=UTF8","root","root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-      // $this->bdd = new PDO("mysql:host=localhost:8889;dbname=grh_v2;charset=UTF8","root","root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
-      // $this->bdd = new PDO("mysql:host=localhost:8889;dbname=grh_sauv070822;charset=UTF8","root","root", [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+      try {
+        $this->bdd = new PDO("mysql:host=".$this->host.":".$this->port.";dbname=".$this->dbname.";charset=UTF8",$this->user,$this->mdp, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+      }
+      catch (PDOException $e) {
+        echo "Echec connexion base de données ".$this->dbname." : ", $e->getMessage();
+        return false;
+        exit();
+      }
+       
     }
 
     public static function getInstance()
@@ -30,6 +36,6 @@ class ConnexionDB {
 
     public function getConnection()
     {
-      return $this->bdd;
+      return $this->bdd;   
     }
 }
